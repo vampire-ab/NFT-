1970a31079c317f9b5155ec16ee549be3a4d0d57
vampire-ab <brijeshagal@gmail.com>
2022-10-19 09:39:06 +0530

    Limited mints per user

--- vampire.sol
+++ vampire.sol
@@ -1,24 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+// Contract Address: 0xF3aF05E672EA23Ae3595D54F975fd82d5286F992
+
 import "@openzeppelin/contracts@4.7.3/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.3/utils/Counters.sol";
 
-contract Vampire is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract Vampire is ERC721, ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    mapping (address => uint8) private _mintCtr;
+    uint256 MAX_AVAILABLE = 1001;
+
 
     constructor() ERC721("vampire", "AB") {}
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, string memory uri) public {
         uint256 tokenId = _tokenIdCounter.current();
+        require(_mintCtr[msg.sender] <= 4, "Sorry, you cannot mint more!");
+        require(tokenId <= MAX_AVAILABLE, "Sorry, we have reached the maximum!");
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        _mintCtr[msg.sender] += 1;
     }
 
     // The following functions are overrides required by Solidity.
